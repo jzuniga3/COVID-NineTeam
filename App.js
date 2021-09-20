3f4386ad3e856c8a70dc19fa670fb5ad0f95cb42
Jovan Zuniga <jzuniga3@ggc.edu>
2021-09-19 20:28:22 -0400

    Adds login and loading messages to screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,6 +10,7 @@ import firebase from 'firebase'
 
 import LandingScreen from './auth/Landing'
 import RegisterScreen from './auth/Register';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBn-HXdCwAxePnognFZ9-4nWpNrJYYfCG8",
@@ -22,19 +23,67 @@ const firebaseConfig = {
   measurementId: "G-ZT4PY1KNGK"
 };
 
+// if firebase is not loaded initialize it
 if(firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig)
 }
 
+// Stack to handle navigation
 const Stack = createStackNavigator();
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default function App() {
-  return (
-    <NavigationContainer>
+    this.state = {
+      loaded: false
+    }
+  }
+
+  // check whether user is logged in
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+
+    // if not loaded display loading screen
+    if(!loaded) {
+      return(
+        <View style={{ flex: 1, justifyContent: 'center'}}>
+          <Text>Loading...</Text>
+        </View>
+      )
+    }
+
+    // if not logged in display navigation container
+    if(!loggedIn) {
+    return (
+      <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}}/>
         <Stack.Screen name="Register" component={RegisterScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
-  );
+    )
+    }
+
+    return(
+      <View style={{ flex: 1, justifyContent: 'center'}}>
+        <Text>User is logged in</Text>
+      </View>
+    )
+  }
 }
