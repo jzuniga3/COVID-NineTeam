8359925de0fedba07cb66c4bf762d3fcaf3bc7e4
Jovan Zuniga <jzuniga3@ggc.edu>
2021-09-27 05:41:43 -0400

    Removes placeholder from profile screen TextInputs to fix crashing

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,21 +1,141 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { StatusBar } from 'expo-status-bar'
 import fire from '../fire'
-import {Text, View, TouchableOpacity} from 'react-native'
+import {Text, View, Button, TextInput} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-const handleLogout = () => {
+const handleLogout = () => 
+{
     fire.auth().signOut()
 }
 
-export default function Profile() {
+export default function Profile()
+{    
+    const [name, setName] = useState("");
+    const [sex, setSex] = useState("");
+    const [age, setAge] = useState(-1);
+    const [weight, setWeight] = useState(-1);
+    const [feet, setFeet] = useState(-1);
+    const [inches, setInches] = useState(-1);
+
+    const usersDB = fire.firestore().collection('users')
+    const userID = fire.auth().currentUser.uid
+    
+    let totalHeight = (feet*12) + inches*1;
+    const BMI = (weight/(totalHeight*totalHeight)*703).toFixed(2);
+
+    const onChangePasswordPress = () =>
+    {
+        usersDB.doc(userID).update({
+            name: {name},
+            age: {age},
+            feet: {feet},
+            inches: {inches},
+            weight: {weight}
+        })
+    }
+
+    //Get user information from firestore
+    usersDB.doc(userID).get().then((doc) => {
+        setName(doc.data().name)
+        setSex(doc.data().sex)
+        setAge(doc.data().age)
+        setWeight(doc.data().weight)
+        setFeet(doc.data().feet)
+        setInches(doc.data().inches)
+    })
+
     return (
-        <SafeAreaView>
+        <SafeAreaView style = {styles.contentCenter}>
+            <StatusBar barStyle='light-content' />
             <Text>Profile</Text>
-            <View>
-                <TouchableOpacity onPress = {handleLogout}>
-                <Text>Logout</Text>
-                </TouchableOpacity>
+            <View style = {styles.profileScreen}>
+
+                <View style = {styles.profileRow}>
+                <Text style = {styles.profileData}>Name:  </Text><TextInput 
+                    style = {styles.profileData}
+                    // placeholder = {name.toString}
+                    onChangeText = {newName => setName({newName})}
+                />
+                </View>
+
+                <View style = {styles.profileRow}>
+                <Text style = {styles.profileData}>Age:  </Text><TextInput 
+                    style = {styles.profileData}
+                    // placeholder = { age.toString }
+                    onChangeText = {newAge => setAge({newAge})}
+                />
+                </View>
+
+                <View style = {styles.profileRow}>
+                <Text style = {styles.profileData}>Height:  </Text><TextInput 
+                    style = {styles.heightInput}
+                    // placeholder = { feet.toString }
+                    onChangeText = {newFeet => setFeet({newFeet})}
+                />
+                <Text style = {styles.profileData}>'  </Text><TextInput 
+                    style = {styles.heightInput}
+                    // placeholder = { inches.toString }
+                    onChangeText = {newInches => setInches({newInches})}
+                />
+                <Text style = {styles.profileData}>"</Text>
+                </View>
+
+                <View style = {styles.profileRow}>
+                <Text style = {styles.profileData}>Weight:  </Text><TextInput 
+                    style = {styles.weightInput}
+                    // placeholder = { weight.toString }
+                    onChangeText = {newWeight => setWeight({newWeight})}
+                />
+                <Text style = {styles.profileData}> lbs</Text>
+                </View>
+
+                <Text style = {styles.profileData}>BMI: {BMI}</Text>
+                <Text>{`\n\n`}</Text>
+
+                <Button
+                    title = 'Save changes'
+                    onPress = {onChangePasswordPress}
+                />
+                <Button
+                    onPress = {handleLogout}
+                    title = 'Logout'
+                />
             </View>
         </SafeAreaView>
-    )
+    );
+}   
+
+const styles =
+{
+    contentCenter:
+    {
+        height: '100%',
+        backgroundColor: "#192879",
+        alignItems: 'center'
+    },
+    profileScreen:
+    {
+        height: '80%',
+        width: '80%',
+        backgroundColor: "#FFFFFF"
+    },
+    profileData:
+    {
+        fontSize: 20,
+    },
+    profileRow:
+    {
+        flexDirection: 'row',
+    },
+    heightInput:
+    {
+        fontSize: 20,
+        width: 25
+    },
+    weightInput:
+    {
+        fontSize: 20,
+        width: 40
+    }
 }
